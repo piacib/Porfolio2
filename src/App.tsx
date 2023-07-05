42b16388795a32c970c3e447dcaf2072cf4558ca
piacib <ben.piacitelli@gmail.com>
2023-07-05 14:03:58 -0700

    moved projects into landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import "./App.scss";
 import "./normalize.css";
 import LandingPage from "./components/LandingPage";
-import Projects from "./components/Projects";
 function App() {
   return (
     <>
       <LandingPage />
-
-      <Projects />
     </>
   );
 }
