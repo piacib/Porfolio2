919147fa6fb4d444b9d9359599b1301efa27fa94
piacib <ben.piacitelli@gmail.com>
2023-07-05 09:17:31 -0700

    added components

--- src/components/LandingPage/index.tsx
+++ src/components/LandingPage/index.tsx
@@ -1,10 +1,33 @@
 import "./style.scss";
 const LandingPage = () => {
   return (
-    <section>
-      <h1>LandingPage</h1>
+    <section className="landing-page_container">
+      <div className="landing-page__left">
+        <WelcomeText />
+        <AboutMeBlurb />
+        <Skills />
+      </div>
+      <CenterCircle />
+      <div className="landing-right"></div>
     </section>
   );
 };
 
+const WelcomeText = () => {
+  return (
+    <h1 className="welcome-header">
+      <span>Welcome To</span>
+      <span>My Portfolio</span>
+    </h1>
+  );
+};
+const AboutMeBlurb = () => {
+  return <></>;
+};
+const Skills = () => {
+  return <></>;
+};
+const CenterCircle = () => {
+  return <></>;
+};
 export default LandingPage;
