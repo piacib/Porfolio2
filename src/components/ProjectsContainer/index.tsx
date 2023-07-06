3c90705d77ae35665dcab76ae2aa9d2cdb8e5dda
piacib <ben.piacitelli@gmail.com>
2023-07-06 08:02:31 -0700

    added scoll to bottom when projects is scrolled

--- src/components/ProjectsContainer/index.tsx
+++ src/components/ProjectsContainer/index.tsx
@@ -4,25 +4,28 @@ import headshot from "../../assets/headshot.png";
 
 const ProjectsContainer = () => {
   return (
-    <div className="projects__container">
+    <div
+      onScroll={() => {
+        // must be last element on page otherwise could scroll past
+        window.scrollTo(0, document.body.scrollHeight);
+      }}
+      className="projects__container y mandatory-scroll-snapping normal-stop"
+    >
       <div className="projects-initial-page__container flex-col">
-        <div className="inner-square inset-shadow ">
-          <img
-            src={headshot}
-            alt="headshot"
-            className="profile-image profile-image__large inset-shadow"
-          />
-        </div>
+        <img
+          src={headshot}
+          alt="headshot"
+          className="profile-image profile-image__large inset-shadow"
+        />
         <a
-          className="project-scroll-button button-flip"
+          className="button-flip"
           data-back="Check Out What I Have Built"
           data-front="Check Out What I Have Built"
-          href="#Pokemon_Showdown_Extension"
-        ></a>
+          href="#Pokeinfo_container"
+        />
       </div>
       <Projects />
     </div>
   );
 };
-
 export default ProjectsContainer;
